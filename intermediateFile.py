7ce61bc0601c246b7db76d0a4c08d239fc918ea6
JorgeCardenas1 <jorgehernancardenas@gmail.com>
2023-10-20 12:01:47 -0500

    adding modules to manage hfss interface

--- intermediateFile.py
+++ intermediateFile.py
@@ -1,7 +1,15 @@
 import ScriptEnv
+import sys
 import os
 sys.path.insert(0, './src/')
-from dbuilder import modules as hfss
+from dbuilder.modules import hfss 
 oDesktop.RestoreWindow()
 oDesktop.OpenProject("C:/Users/jorge/Documents/Projects Jorge C/DRUIDA PROJECT/POC/dbGeneration_v0/Models/testing-multioutput/meta-atom_05_datageneration.aedt")
 oProject = oDesktop.SetActiveProject("meta-atom_05_datageneration")
+hfss.setVariable(oProject,"radio ","1.3mm","HFSSDesign1")
+oDesign = oProject.SetActiveDesign("HFSSDesign1")
+oDesign.AnalyzeAll()
+oModule = oDesign.GetModule("OutputVariable")
+oModule.CreateOutputVariable("ReflectanceTE", "(mag(S(FloquetPort1:1,FloquetPort1:1)))^2", "Setup1 : Sweep", "Modal Solution Data", [])
+oModule.CreateOutputVariable("ReflectanceTM", "(mag(S(FloquetPort1:2,FloquetPort1:2)))^2", "Setup1 : Sweep", "Modal Solution Data", [])
+hfss.createResult(oProject,'C:\Users\jorge\Documents\Projects Jorge C\DRUIDA PROJECT\POC\dbGeneration_v0\Exports','_0_0','model','anbshkd99384m')
--- src/dbuilder/Manager.py
+++ src/dbuilder/Manager.py
@@ -29,7 +29,7 @@ class Builder:
 
         os.chdir( os.path.normpath(filePath))
         drawing = '"' + self.modelPath + self.projectName + '.aedt"'
-
+        print(drawing)
 
         f = open("intermediateFile.py", "w")   #abre un archivo para escribir
 
@@ -39,24 +39,25 @@ class Builder:
 
 
         f.write("import ScriptEnv\n")
+        f.write("import sys\n")
         f.write("import os\n")
         f.write("sys.path.insert(0, './src/')\n")
-        f.write("from dbuilder import modules as hfss\n")
+        f.write("from dbuilder.modules import hfss \n")
 
 
         f.write("oDesktop.RestoreWindow()\n")
         f.write("oDesktop.OpenProject(" + drawing + ")\n")
         f.write("oProject = oDesktop.SetActiveProject(" + '"'+self.projectName+'"' + ")\n")
-        #f.write('hfss.setVariable(oProject,"' + variableName + ' ","' + value + units + '","' +self.designName +'")\n')
+        f.write('hfss.setVariable(oProject,"' + variableName + ' ","' + value + units + '","' +self.designName +'")\n')
         
-        #f.write('oDesign = oProject.SetActiveDesign("' +self.designName  + '")\n')
-        #f.write("oDesign.AnalyzeAll()")
-        #f.write("\n")
-        #f.write('oModule = oDesign.GetModule("OutputVariable")\n')
-        #f.write('oModule.CreateOutputVariable("ReflectanceTE", "(mag(S(FloquetPort1:1,FloquetPort1:1)))^2", "Setup1 : Sweep", "Modal Solution Data", [])\n')
-        #f.write('oModule.CreateOutputVariable("ReflectanceTM", "(mag(S(FloquetPort1:2,FloquetPort1:2)))^2", "Setup1 : Sweep", "Modal Solution Data", [])\n')
+        f.write('oDesign = oProject.SetActiveDesign("' +self.designName  + '")\n')
+        f.write("oDesign.AnalyzeAll()")
+        f.write("\n")
+        f.write('oModule = oDesign.GetModule("OutputVariable")\n')
+        f.write('oModule.CreateOutputVariable("ReflectanceTE", "(mag(S(FloquetPort1:1,FloquetPort1:1)))^2", "Setup1 : Sweep", "Modal Solution Data", [])\n')
+        f.write('oModule.CreateOutputVariable("ReflectanceTM", "(mag(S(FloquetPort1:2,FloquetPort1:2)))^2", "Setup1 : Sweep", "Modal Solution Data", [])\n')
 
-        #f.write("hfss.createResult(oProject,'" + self.exportPath +"','"+ tag +"','"+ self.modelName  +"','"+ simulationID  +"')\n")
+        f.write("hfss.createResult(oProject,'" + self.exportPath +"','"+ tag +"','"+ self.modelName  +"','"+ simulationID  +"')\n")
         f.close()
 
 
